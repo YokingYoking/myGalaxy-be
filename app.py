f4fbb0e5c0d8ff8452a8ae6777982b91d434795c
zhaoyujian <603245498@qq.com>
2023-03-15 16:31:26 +0800

    complete simple demo

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
 from bioblend import galaxy
+from flask_cors import CORS
 
 app = Flask(__name__)  # __name__代表当前app.py这个模块
-
+CORS(app, resources=r'/*')
 gi = galaxy.GalaxyInstance(url='https://usegalaxy.org/', key='5708d9d457c6681669c3ad03027263ba')
-
-hl = gi.histories.get_histories()
-hist = gi.histories.list()[0]
-inputs = hist.get_datasets()[:2]
+hi = gi.histories
+di = gi.datasets
+ti = gi.tools
 
 
 @app.route('/')
@@ -17,15 +17,32 @@ def hello_world():  # put application's code here
 
 @app.route('/getHistory')
 def get_history():
-    return hl
+    return hi.get_histories()
 
 
 @app.route('/getDataset')
 def get_dataset():
-    return inputs
+    return di.get_datasets(100, 0)
 
 
-# example
+@app.route('/downloadDataset')
+def download_dataset():
+    did = request.args.get("id", type=str)
+    return di.download_dataset(dataset_id=did)
+
+
+@app.route('/showHistory')
+def show_history():
+    hid = request.args.get("id", type=str)
+    hi.show_history(history_id=hid)
+
+
+@app.route('/getTools')
+def get_tools():
+    return ti.get_tools()
+
+
+# examples
 @app.route('/blog/<int:blog_id>')
 def blog_detail(blog_id):
     return "Blog Id is: %s" % blog_id
